f3543e8dcc2a037e5637c244b2204625e9558668
Greembow <greembow@outlook.com>
2022-02-13 21:08:53 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# google_classroom_assignment_list
+Badly written by greembow in less than a day
+
+---
+Hi, this is a simple program written in python to use the google classroom api to list out your assignments. Missing assignments are listed in big, scary red as well as have a prefix of `Late:` so they can be copied and pasted into something from your terminal.
+
+## Installation:
+- Clone the repo
+- Follow the [Google Classroom API's Quickstart Guide](https://developers.google.com/classroom/quickstart/python#prerequisites) to install the required dependencies and create credentials for the API
+- Place your `credentials.json` file in the root directory of the repo
+- Run the program and authenticate with Google
+- A `token.json` file should be created and your assignments will start to slowly list
+
+## Issues:
+- Assignments list slowly
+- Shoddy code
+- Archived classes show up in the list (this is a limitation of the API)
+
+My response: I am a rust developer and made this in a very short period of time. Commits are welcome if you want to add new features/clean up my code!
---
+++ src/main.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from colorama import Fore, Style
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly', 'https://www.googleapis.com/auth/classroom.coursework.me']
+
+
+def main():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('./token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('classroom', 'v1', credentials=creds)
+
+        # Call the Classroom API
+        results = service.courses().list().execute()
+        courses = results.get('courses', [])
+
+        if not courses: # if no courses are found
+            print(Fore.RED+'No courses found')
+            return # exit
+        for course in courses: # for each course in the list
+            print(course.get('name') + ':') # print the name of the course with a semicolon
+            course_work_results=service.courses().courseWork().list(courseId=course.get('id')).execute() # get a list of the assignments
+            for assignment in course_work_results['courseWork']: # for each assignment
+                student_submissions = service.courses().courseWork().studentSubmissions().list(courseId=course.get('id'), courseWorkId=assignment['id']).execute() # get a list of student submissions, so we can check for if it is late/turned in
+                for submission in student_submissions['studentSubmissions']: # for each submission (should only be one but this safeguards it)
+                    if submission['state'] == 'CREATED': # if the state is CREATED it means not turned in
+                        try: # classroom's api does not include the late parameter unless it is true, so abuse a try statement here
+                            if submission['late']: # if the assignment is late
+                                print(Fore.RED+'    Late: '+assignment['title']+Style.RESET_ALL) # print the assignment title in a scary colour
+                        except: # when it fails because the assignment is not late
+                            print('    '+assignment['title']) # print assignment name
+            
+    except HttpError as error: # oops
+        print('An error occurred: %s' % error)
+submission
+
+if __name__ == '__main__':
+    main()
